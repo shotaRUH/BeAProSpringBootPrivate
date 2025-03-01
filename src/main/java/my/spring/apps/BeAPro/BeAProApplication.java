9cda9ca6173cb5cf69c9a7578bcfe3075306f7e5
shotaRUH <sadeeenadeee@gmail.com>
2025-03-01 10:56:18 +0530

    +contribution ahh ahh ah

--- src/main/java/my/spring/apps/BeAPro/BeAProApplication.java
+++ src/main/java/my/spring/apps/BeAPro/BeAProApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeAProApplication {
 
 	public static void main(String[] args) {
+		int aaaaa=1000;
 		SpringApplication.run(BeAProApplication.class, args);
+		int b=100;
 	}
 
 }
